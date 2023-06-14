43ff759ae6d070178ca4d47caa8913219acc10c2
Pavel <belousov-p9163@bk.ru>
2023-06-14 21:57:35 +0300

    add code in project

---
+++ Home2.java
@@ -0,0 +1,25 @@
+public class Home2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово: ");
+        String word = scanner.nextLine();
+
+        boolean isPalindrome = checkPalindrome(word);
+        System.out.println(isPalindrome);
+    }
+
+    public static boolean checkPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
