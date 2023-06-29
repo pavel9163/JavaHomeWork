2acdb2d977ab93a3ddc79b5a960f33ffd8c8a05b
Павел <belousov-p9163@bk.ru>
2023-06-29 19:04:52 +0300

    add code for home4

--- Home3.java
+++ Home3.java
@@ -3,79 +3,79 @@ import java.util.Scanner;
 
 public class ArrayListUtils {
 
-public static void removeEven(ArrayList<Integer> list) {
-list.removeIf(num -> num % 2 == 0);
-}
+    public static void removeEven(ArrayList<Integer> list) {
+        list.removeIf(num -> num % 2 == 0);
+    }
 
-public static Integer findMin(ArrayList<Integer> list) {
-if (list.isEmpty()) {
-throw new IllegalArgumentException("The list is empty.");
-}
+    public static Integer findMin(ArrayList<Integer> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("The list is empty.");
+        }
 
-Integer min = list.get(0);
-for (Integer num : list) {
-if (num < min) {
-min = num;
-}
-}
+        Integer min = list.get(0);
+        for (Integer num : list) {
+            if (num < min) {
+                min = num;
+            }
+        }
 
-return min;
-}
+        return min;
+    }
 
-public static Integer findMax(ArrayList<Integer> list) {
-if (list.isEmpty()) {
-throw new IllegalArgumentException("The list is empty.");
-}
+    public static Integer findMax(ArrayList<Integer> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("The list is empty.");
+        }
 
-Integer max = list.get(0);
-for (Integer num : list) {
-if (num > max) {
-max = num;
-}
-}
+        Integer max = list.get(0);
+        for (Integer num : list) {
+            if (num > max) {
+                max = num;
+            }
+        }
 
-return max;
-}
+        return max;
+    }
 
-public static double findAverage(ArrayList<Integer> list) {
-if (list.isEmpty()) {
-throw new IllegalArgumentException("The list is empty.");
-}
+    public static double findAverage(ArrayList<Integer> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("The list is empty.");
+        }
 
-int sum = 0;
-for (Integer num : list) {
-sum += num;
-}
+        int sum = 0;
+        for (Integer num : list) {
+            sum += num;
+        }
 
-return (double) sum / list.size();
-}
+        return (double) sum / list.size();
+    }
 
-public static void main(String[] args) {
-ArrayList<Integer> numbers = new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter the size of the array: ");
-int size = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
 
-System.out.println("Enter the elements of the array:");
-for (int i = 0; i < size; i++) {
-int num = scanner.nextInt();
-numbers.add(num);
-}
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            int num = scanner.nextInt();
+            numbers.add(num);
+        }
 
-System.out.println("Before removing even numbers: " + numbers);
+        System.out.println("Before removing even numbers: " + numbers);
 
-removeEven(numbers);
+        removeEven(numbers);
 
-System.out.println("After removing even numbers: " + numbers);
+        System.out.println("After removing even numbers: " + numbers);
 
-Integer min = findMin(numbers);
-System.out.println("Minimum value: " + min);
+        Integer min = findMin(numbers);
+        System.out.println("Minimum value: " + min);
 
-Integer max = findMax(numbers);
-System.out.println("Maximum value: " + max);
+        Integer max = findMax(numbers);
+        System.out.println("Maximum value: " + max);
 
-double average = findAverage(numbers);
-System.out.println("Average value: " + average);
-}
+        double average = findAverage(numbers);
+        System.out.println("Average value: " + average);
+    }
 }
---
+++ Home4.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Home4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<String> list = new LinkedList<>();
+
+        while (true) {
+            System.out.println("Введите строку (print/revert для выполнения команд):");
+            String input = scanner.nextLine();
+
+            if (input.equals("print")) {
+                System.out.println("Строки в обратном порядке:");
+                while (!list.isEmpty()) {
+                    System.out.println(list.removeLast());
+                }
+            } else if (input.equals("revert")) {
+                if (!list.isEmpty()) {
+                    list.removeLast();
+                    System.out.println("Последняя введенная строка удалена из памяти.");
+                } else {
+                    System.out.println("Нет сохраненных строк для удаления.");
+                }
+            } else {
+                list.addLast(input);
+            }
+        }
+    }
+}
